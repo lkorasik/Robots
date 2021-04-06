7cab6edbb5ab15b6c97873ba37c9623bce55b21a
obduvan <obduvann@gmail.com>
2021-04-07 01:18:23 +0500

    Revert "fixed all world problems (God)"
    This reverts commit 93849ec4

--- robots/src/main/java/view/menu/MenuInternalItem.java
+++ robots/src/main/java/view/menu/MenuInternalItem.java
@@ -11,18 +11,22 @@ public class MenuInternalItem extends JMenuItem {
     public MenuInternalItem(String title, int keyEvent, TypeMenuItem typeMenuItem, MainAppFrame mainAppFrame) {
         super(title, keyEvent);
         switch (typeMenuItem) {
-            case EXIT -> addActionListener(event ->
-                    mainAppFrame.dispatchEvent(new WindowEvent(mainAppFrame, WindowEvent.WINDOW_CLOSING)));
-            case LOG -> addActionListener(event ->
-                    Logger.debug(Constants.MainApplicationFrame.LOG_MESSAGE));
-            case THEME_SYSTEM -> addActionListener(event -> {
-                setLookAndFeel(UIManager.getSystemLookAndFeelClassName(), mainAppFrame);
-                mainAppFrame.invalidate();
-            });
-            case THEME_UNIVERSAL -> addActionListener(event -> {
-                setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName(), mainAppFrame);
-                mainAppFrame.invalidate();
-            });
+            case EXIT:
+                addActionListener(event ->
+                        mainAppFrame.dispatchEvent(new WindowEvent(mainAppFrame, WindowEvent.WINDOW_CLOSING)));
+            case LOG:
+                addActionListener(event ->
+                        Logger.debug(Constants.MainApplicationFrame.LOG_MESSAGE));
+            case THEME_SYSTEM:
+                addActionListener(event -> {
+                    setLookAndFeel(UIManager.getSystemLookAndFeelClassName(), mainAppFrame);
+                    mainAppFrame.invalidate();
+                });
+            case THEME_UNIVERSAL:
+                addActionListener(event -> {
+                    setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName(), mainAppFrame);
+                    mainAppFrame.invalidate();
+                });
         }
     }
 
