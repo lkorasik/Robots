f5b2fc8d8de3a1ef120b8705e5ec5cd25952aa15
obduvan <obduvann@gmail.com>
2021-05-18 00:21:43 +0500

    init #1

---
+++ robots/src/main/java/controller/enemyController/EnemyController.java
@@ -0,0 +1,6 @@
+package controller.enemyController;
+
+public class EnemyController {
+
+
+}
--- robots/src/main/java/model/enemyModel/EnemyLogic.java
+++ robots/src/main/java/model/enemyModel/EnemyLogic.java
@@ -2,58 +2,30 @@ package model.enemyModel;
 
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 public class EnemyLogic {
-
     private volatile double robotDirection = 0;
+    private final Point2D robotPosition = new Point2D.Double(0, 0);
+    private final Point2D targetPosition = new Point2D.Double(0, 0);
 
-    private final Point position = new Point(100, 100);
-    private final Point seePoint = new Point(100, 100);
-    private final Point predPosition = new Point(100, 100);
-
-    public int getPositionY() {
-        return position.y;
-    }
-
-    public int getPositionX() {
-        return position.x;
-    }
-
-    public double getRobotDirection(){
+    public double getRobotDirection() {
         return robotDirection;
     }
 
-    public Point getPositionPoint() {
-        return position;
+    public Point2D getTargetPosition() {
+        return targetPosition;
     }
 
-    public static int round(double value) {
-        return (int) (value + 0.5);
+    public Point2D getRobotPosition() {
+        return robotPosition;
     }
 
-    public void setMovePosition(Point point) {
-        predPosition.x = point.x;
-        predPosition.y = point.y;
-    }
 
-    public void setSeePosition(Point point) {
-        seePoint.x = point.x;
-        seePoint.y = point.y;
-        robotDirection = angleTo(position.x, position.y, seePoint.x, seePoint.y);
-    }
-
-    public void onModelUpdateEvent(int width, int height) {
-        if (predPosition.x > width)
-            predPosition.x = width;
-        else if (predPosition.x < 0)
-            predPosition.x = 0;
-        else if (predPosition.y < 0)
-            predPosition.y = 0;
-        else if (predPosition.y > height)
-            predPosition.y = height;
-        else
-            moveRobot();
-
+    private static double distance(double x1, double y1, double x2, double y2) {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
     }
 
     private static double angleTo(double fromX, double fromY, double toX, double toY) {
@@ -63,10 +35,30 @@ public class EnemyLogic {
         return asNormalizedRadians(Math.atan2(diffY, diffX));
     }
 
-    private void moveRobot() {
-        robotDirection = angleTo(position.x, position.y, seePoint.x, seePoint.y);
-        position.x = predPosition.x;
-        position.y = predPosition.y;
+    private void moveRobot(double angularVelocity, int width, int height) {
+        var newDirection = robotDirection + angularVelocity * Constants.Robot.DEFAULT_DURATION;
+
+        double newX = robotPositionX + Constants.Robot.MAX_VELOCITY / angularVelocity * (Math.sin(newDirection) - Math.sin(robotDirection));
+        if (!Double.isFinite(newX)) {
+            newX = robotPositionX + Constants.Robot.MAX_VELOCITY * Constants.Robot.DEFAULT_DURATION * Math.cos(robotDirection);
+        }
+
+        double newY = robotPositionY - Constants.Robot.MAX_VELOCITY / angularVelocity * (Math.cos(newDirection) - Math.cos(robotDirection));
+        if (!Double.isFinite(newY)) {
+            newY = robotPositionY + Constants.Robot.MAX_VELOCITY * Constants.Robot.DEFAULT_DURATION * Math.sin(robotDirection);
+        }
+
+        robotPositionX = newX;
+        robotPositionY = newY;
+        robotDirection = asNormalizedRadians(correctDirection(newDirection, width, height));
+    }
+
+    private double correctDirection(double direction, int width, int height) {
+        if ((robotPositionX <= 0) || (robotPositionY >= height))
+            return direction + Math.PI;
+        else if ((robotPositionX >= width) || (robotPositionY <= 0))
+            return direction - Math.PI;
+        return direction;
     }
 
     private static double asNormalizedRadians(double angle) {
@@ -78,4 +70,56 @@ public class EnemyLogic {
         }
         return angle;
     }
+
+    public static int round(double value) {
+        return (int) (value + 0.5);
+    }
+
+    public void setTargetPosition(Point point) {
+        targetPositionX = point.x;
+        targetPositionY = point.y;
+    }
+
+    private double getAngularVelocity() {
+        double angularVelocity = 0;
+        double angleToTarget = angleTo(robotPositionX, robotPositionY, targetPositionX, targetPositionY);
+        if (angleToTarget > robotDirection) {
+            angularVelocity = Constants.Robot.MAX_ANGULAR_VELOCITY;
+        }
+        if (angleToTarget < robotDirection) {
+            angularVelocity = -Constants.Robot.MAX_ANGULAR_VELOCITY;
+        }
+        return angularVelocity;
+    }
+
+    private void keepTargetInRectangle(int width, int height) {
+        if (targetPositionX > width)
+            targetPositionX = width;
+        else if (targetPositionX < 0)
+            targetPositionX = 0;
+        else if (targetPositionY < 0)
+            targetPositionY = 0;
+        else if (targetPositionY > height)
+            targetPositionY = height;
+    }
+
+    public void onModelUpdateEvent(int width, int height) {
+        keepTargetInRectangle(width, height);
+
+        double distance = distance(targetPositionX, targetPositionY, robotPositionX, robotPositionY);
+        if (distance < 0.5)
+            return;
+        if (robotPositionX > width)
+            robotPositionX = width;
+        else if (robotPositionX < 0)
+            robotPositionX = 0;
+        else if (robotPositionY < 0)
+            robotPositionY = 0;
+        else if (robotPositionY > height)
+            robotPositionY = height;
+        else
+            moveRobot(getAngularVelocity(), width, height);
+    }
+
+
 }
